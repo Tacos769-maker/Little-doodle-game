905c0bdb069c8897082f14d5614aff44927b7545
Tacos769-maker <jacobtacos769@gmail.com>
2026-08-03 23:07:51 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
 import os
-from flask import Flask, render_template
+import json
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-# Shared server-side world state
-world_blocks = {}
+DATA_FILE = 'world.json'
+
+def load_world_from_disk():
+    if os.path.exists(DATA_FILE):
+        try:
+            with open(DATA_FILE, 'r') as f:
+                return json.load(f)
+        except:
+            return {}
+    return {}
+
+def save_world_to_disk(world):
+    with open(DATA_FILE, 'w') as f:
+        json.dump(world, f)
 
 @app.route('/')
 def index():
@@ -15,7 +28,7 @@ def index():
 
 @socketio.on('connect')
 def handle_connect():
-    # Send the current world state immediately to the newly joined player
+    world_blocks = load_world_from_disk()
     emit('load_world', world_blocks)
 
 @socketio.on('place_block')
@@ -25,24 +38,25 @@ def handle_place_block(data):
     color = data['color']
     key = f"{x},{y}"
     
+    world_blocks = load_world_from_disk()
+    
     if color is None:
         if key in world_blocks:
             del world_blocks[key]
     else:
         world_blocks[key] = color
         
-    # Broadcast the change to ALL connected players instantly
+    save_world_to_disk(world_blocks)
+        
     emit('update_block', {'x': x, 'y': y, 'color': color}, broadcast=True)
 
 @socketio.on('clear_board')
 def handle_clear():
-    global world_blocks
-    world_blocks.clear()
+    save_world_to_disk({})
     emit('clear_board', broadcast=True)
 
 @socketio.on('mouse_move')
 def handle_mouse_move(data):
-    # Broadcast cursor positions to other players
     emit('update_cursor', {'id': request.sid, 'x': data['x'], 'y': data['y'], 'color': data['color']}, broadcast=True)
 
 if __name__ == '__main__':
